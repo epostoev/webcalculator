960d929403e02d839e477cbb5f0bb01df74f1b16
Postoev Evgeniy <e.postoev@office.elvis.ru>
2024-10-08 16:58:24 +0300

    04_add db*

--- 02_db/db_class_classes.sql
+++ 02_db/db_class_classes.sql
@@ -29,7 +29,7 @@ CREATE TABLE `classes` (
   `parametr_2` varchar(255) DEFAULT NULL,
   `parametr_3` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`class_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -51,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-07 15:42:20
+-- Dump completed on 2024-10-08 16:55:29
---
+++ 02_db/db_class_solution_of_venders_avz.sql
@@ -0,0 +1,51 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_class
+-- ------------------------------------------------------
+-- Server version	9.0.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `solution_of_venders_avz`
+--
+
+DROP TABLE IF EXISTS `solution_of_venders_avz`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `solution_of_venders_avz` (
+  `avz_id` int NOT NULL AUTO_INCREMENT,
+  `solution_of_venders` varchar(255) NOT NULL,
+  PRIMARY KEY (`avz_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solution_of_venders_avz`
+--
+
+LOCK TABLES `solution_of_venders_avz` WRITE;
+/*!40000 ALTER TABLE `solution_of_venders_avz` DISABLE KEYS */;
+INSERT INTO `solution_of_venders_avz` VALUES (1,'Kaspersky Security для виртуальных и облачных сред'),(2,'KICS for Nodes Windows'),(3,'KICS for Nodes Linux'),(4,'KES Windows'),(5,'KES Linux'),(6,'PT MultiScanner'),(7,'Dr Web Security Suite'),(8,'Dr Web Industrial');
+/*!40000 ALTER TABLE `solution_of_venders_avz` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-08 16:55:29
